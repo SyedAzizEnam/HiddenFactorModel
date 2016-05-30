b2586c3c8fb2dd83acb0f487f4596dcd42c78668
shiladitya <shiladis@uci.edu>
2016-05-29 18:40:03 -0700

    Added function to HFT to get word-topic frequencies

--- lda/HFT.py
+++ lda/HFT.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.sparse import lil_matrix
+
 from RatingModel import RatingModel
 from ReviewModel import ReviewModel
 
@@ -8,3 +11,15 @@ class HFT:
         self.rating_model = RatingModel(ratings_filename, n_hidden)
         self.review_model = ReviewModel(reviews_filename, n_hidden)
         self.kappa = 0.0
+
+    def get_theta(self):
+        self.review_model.theta = np.exp(self.kappa*self.rating_model.gamma_item)
+        partition = np.sum(self.review_model.theta, axis=1)
+        self.review_model.theta /= partition[:, None]
+
+    def get_word_topic_frequencies(self):
+        frequencies = np.ndarray((self.review_model.n_topics, self.review_model.n_vocab), dtype=int)
+        for review, topics in zip(self.review_model.reviews, self.review_model.z):
+            for word, topic in zip(review, topics):
+                frequencies[topic, word] += 1
+        return frequencies
--- lda/ReviewModel.py
+++ lda/ReviewModel.py
@@ -2,16 +2,39 @@ import numpy as np
 from DataProcessor import DataLoader
 
 
+def flatten_bow(bow_review):
+    """
+    Flattens the bag of words array so that it is the
+    same shape as the topic assingment array (z)
+
+    Parameters
+    ----------
+    bow_review
+
+    Returns
+    -------
+    words
+
+    """
+    indices = np.flatnonzero(bow_review)
+    words = []
+    for j in indices:
+        words += [j]*int(bow_review[j])
+    return np.array(words)
+
+
 class ReviewModel:
-
     def __init__(self, reviews_filename='../Data/reviews.npz', n_topics=10):
-        self.data = DataLoader.review_data(reviews_filename)
-
-        n_docs, n_vocab = self.data.shape
-        self.phi = np.zeros((n_topics, n_vocab))
-        self.theta = np.zeros((n_docs, n_topics))
+        data = DataLoader.review_data(reviews_filename)
+        self.n_docs, self.n_vocab = data.shape
+        self.n_topics = n_topics
+        self.phi = np.zeros((n_topics, self.n_vocab))
+        self.theta = np.zeros((self.n_docs, n_topics))
 
         self.z = list()
-        for doc_ix in xrange(n_docs):
-            n_words = int(np.sum(self.data[doc_ix, :].toarray()))
+        self.reviews = list()
+        for doc_ix in xrange(self.n_docs):
+            data_review = data[doc_ix, :].toarray()[0]
+            n_words = int(np.sum(data_review))
             self.z.append(np.zeros(n_words, dtype=int))
+            self.reviews.append(flatten_bow(data_review))
