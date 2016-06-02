71491f61ec8fec3b9c7649a09f06ab080b6dddea
shiladitya <shiladis@uci.edu>
2016-06-01 17:27:40 -0700

    Completed gradients; No tests run

--- lda/HFT.py
+++ lda/HFT.py
@@ -5,11 +5,11 @@ from ReviewModel import ReviewModel
 
 
 class HFT:
-
     def __init__(self, ratings_filename='../Data/ratings.npz', reviews_filename='../Data/reviews.npz', n_hidden=10):
         self.rating_model = RatingModel(ratings_filename, n_hidden)
         self.review_model = ReviewModel(reviews_filename, n_hidden)
         self.kappa = 0.0
+        self.mu = 1.0
 
     def get_theta(self):
         self.review_model.theta = np.exp(self.kappa * self.rating_model.gamma_item)
@@ -33,16 +33,21 @@ class HFT:
         return 2 * np.dot((self.rating_model.predicted_rating - self.rating_model.data),
                           self.rating_model.gamma_item)
 
-    def get_kappa_gradient(self):
-        kappa_gradient = 0
-        for b_ix in xrange(self.review_model.n_docs):
-            topics = self.review_model.z[b_ix]
-            kappa_gradient += np.sum(self.rating_model.gamma_item[b_ix, topics] *
-                                     (1 - self.review_model.theta[b_ix, topics]))
-            return kappa_gradient
+    def get_gamma_item_gradients(self):
+        return 2 * np.dot((self.rating_model.predicted_rating - self.rating_model.data),
+                          self.rating_model.gamma_item) - self.mu * self.kappa * \
+                                                          (self.review_model.topic_frequencies -
+                                                           self.review_model.theta *
+                                                           np.sum(self.review_model.topic_frequencies, axis=1)[:, None]
+                                                           )
 
     def get_alpha_gradient(self):
-        return 2*np.sum(self.rating_model.predicted_rating - self.rating_model.data)
+        return 2 * np.sum(self.rating_model.predicted_rating - self.rating_model.data)
 
     def get_phi_gradient(self):
         return np.divide(self.get_word_topic_frequencies(), self.review_model.phi)
+
+    def get_kappa_gradient(self):
+        return np.sum(self.rating_model.gamma_item * (self.review_model.topic_frequencies -
+                                                      self.review_model.theta *
+                                                      np.sum(self.review_model.topic_frequencies, axis=1)[:, None]))
--- lda/RatingModel.py
+++ lda/RatingModel.py
@@ -1,5 +1,6 @@
 import numpy as np
 from DataProcessor import DataLoader
+from scipy.sparse import csr_matrix
 
 
 class RatingModel:
