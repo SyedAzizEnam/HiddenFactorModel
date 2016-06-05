Collected gradients together; Bugs to be fixed

lda/HFT.py
@@ -7,7 +7,11 @@ from ReviewModel import ReviewModel
 class HFT:
     def __init__(self, ratings_filename='../Data/ratings.npz', reviews_filename='../Data/reviews.npz', n_hidden=10):
         self.rating_model = RatingModel(ratings_filename, n_hidden)
+        self.rating_model.get_predicted_ratings()
+
         self.review_model = ReviewModel(reviews_filename, n_hidden)
+        self.review_model.Gibbsampler()
+
         self.kappa = 0.0
         self.mu = 1.0
 
@@ -16,38 +20,47 @@ class HFT:
         partition = np.sum(self.review_model.theta, axis=1)
         self.review_model.theta /= partition[:, None]
 
-    def get_word_topic_frequencies(self):
-        frequencies = np.ndarray((self.review_model.n_topics, self.review_model.n_vocab), dtype=int)
-        for review, topics in zip(self.review_model.reviews, self.review_model.z):
-            for word, topic in zip(review, topics):
-                frequencies[topic, word] += 1
-        return frequencies
+    # def get_word_topic_frequencies(self):
+    #     frequencies = np.ndarray((self.review_model.n_topics, self.review_model.n_vocab), dtype=int)
+    #     for review, topics in zip(self.review_model.reviews, self.review_model.z):
+    #         for word, topic in zip(review, topics):
+    #             frequencies[topic, word] += 1
+    #     return frequencies
 
-    def get_beta_item_gradients(self):
-        return 2 * np.sum(self.rating_model.predicted_rating - self.rating_model.data, axis=0)
+    # def get_beta_item_gradients(self, rating_loss):
+    #     return 2 * np.sum(rating_loss, axis=0)
+    #
+    # def get_beta_user_gradients(self, rating_loss):
+    #     return 2 * np.sum(rating_loss, axis=1)
+    #
+    # def get_gamma_user_gradients(self, rating_loss):
+    #     return 2 * np.dot(rating_loss, self.rating_model.gamma_item)
+    #
+    # def get_gamma_item_gradients(self, rating_loss, review_loss):
+    #     return 2 * np.dot(rating_loss,
+    #                       self.rating_model.gamma_item) - self.mu * self.kappa * review_loss
+    #
+    # def get_alpha_gradient(self, rating_loss):
+    #     return 2 * np.sum(rating_loss)
+    #
+    # def get_phi_gradients(self):
+    #     return np.divide(self.get_word_topic_frequencies(), self.review_model.phi)
+    #
+    # def get_kappa_gradient(self, review_loss):
+    #     return np.sum(self.rating_model.gamma_item * review_loss)
 
-    def get_beta_user_gradients(self):
-        return 2 * np.sum(self.rating_model.predicted_rating - self.rating_model.data, axis=1)
+    def get_gradients(self):
+        rating_loss = self.rating_model.predicted_rating - self.rating_model.data
+        review_loss = (self.review_model.topic_frequencies -
+                       self.review_model.theta * np.sum(self.review_model.topic_frequencies, axis=1)[:, None])
 
-    def get_gamma_user_gradients(self):
-        return 2 * np.dot((self.rating_model.predicted_rating - self.rating_model.data),
-                          self.rating_model.gamma_item)
+        alpha_gradient = 2 * np.sum(rating_loss)
+        beta_item_gradients = 2 * np.sum(rating_loss, axis=0)
+        beta_user_gradients = 2 * np.sum(rating_loss, axis=1)
+        gamma_user_gradients = 2 * np.dot(rating_loss, self.rating_model.gamma_item)
+        gamma_item_gradients = 2 * np.dot(rating_loss, self.rating_model.gamma_item) - self.mu*self.kappa*review_loss
+        phi_gradients = np.divide(self.get_word_topic_frequencies(), self.review_model.phi)
+        kappa_gradient = np.sum(self.rating_model.gamma_item * review_loss)
 
-    def get_gamma_item_gradients(self):
-        return 2 * np.dot((self.rating_model.predicted_rating - self.rating_model.data),
-                          self.rating_model.gamma_item) - self.mu * self.kappa * \
-                                                          (self.review_model.topic_frequencies -
-                                                           self.review_model.theta *
-                                                           np.sum(self.review_model.topic_frequencies, axis=1)[:, None]
-                                                           )
-
-    def get_alpha_gradient(self):
-        return 2 * np.sum(self.rating_model.predicted_rating - self.rating_model.data)
-
-    def get_phi_gradient(self):
-        return np.divide(self.get_word_topic_frequencies(), self.review_model.phi)
-
-    def get_kappa_gradient(self):
-        return np.sum(self.rating_model.gamma_item * (self.review_model.topic_frequencies -
-                                                      self.review_model.theta *
-                                                      np.sum(self.review_model.topic_frequencies, axis=1)[:, None]))
+        return [alpha_gradient, beta_user_gradients, beta_item_gradients,
+                gamma_user_gradients, gamma_item_gradients, phi_gradients, kappa_gradient]

lda/RatingModel.py
@@ -1,6 +1,5 @@
 import numpy as np
 from DataProcessor import DataLoader
-from scipy.sparse import csr_matrix
 
 
 class RatingModel:
@@ -11,17 +10,21 @@ class RatingModel:
         self.corpus_ix = (self.data > 0).toarray()
 
         self.alpha = 0.0
-        self.beta_user = np.zeros(n_users)
-        self.beta_item = np.zeros(n_items)
-        self.gamma_user = np.zeros((n_users, n_hidden_factors))
-        self.gamma_item = np.zeros((n_items, n_hidden_factors))
-        self.predicted_rating = None
+        self.beta_user = np.random.rand(n_users)
+        self.beta_item = np.random.rand(n_items)
+        self.gamma_user = np.random.rand(n_users, n_hidden_factors)
+        self.gamma_item = np.random.rand(n_items, n_hidden_factors)
+        self.predicted_rating = np.zeros((n_users, n_items))
 
     def get_predicted_ratings(self):
-        self.predicted_rating = self.alpha + self.beta_user[:, None] + \
-              self.beta_item + np.dot(self.gamma_user, self.gamma_item.transpose())
+        # self.predicted_rating = self.alpha + self.beta_user[:, None] + \
+        #       self.beta_item + np.dot(self.gamma_user, self.gamma_item.transpose())
+        # self.predicted_rating[np.logical_not(self.corpus_ix)] = 0
+        ix = self.data.nonzero()
+        for u, i in zip(ix[0], ix[1]):
+            self.predicted_rating[u, i] = np.dot(self.gamma_user[u, :], self.gamma_item[i, :])
+        self.predicted_rating += self.alpha + self.beta_user[:, None] + self.beta_item
         self.predicted_rating[np.logical_not(self.corpus_ix)] = 0
 
     def get_rating_error(self):
-        corpus_ix = self.data.nonzero()
         return np.sum(np.square(self.predicted_rating - self.data))

lda/ReviewModel.py
@@ -31,11 +31,11 @@ class ReviewModel:
         self.n_docs, self.n_vocab = data.shape
         self.n_topics = n_topics
 
-        self.phi = np.random.rand((n_topics, self.n_vocab))
-        self.phi = self.phi/self.phi.sum(axis=1)[:,None]
+        self.phi = np.random.rand(n_topics, self.n_vocab)
+        self.phi = self.phi/self.phi.sum(axis=1)[:, None]
 
-        self.theta = np.random.rand((self.n_docs, n_topics))
-        self.theta = self.theta/self.theta.sum(axis=1)[:,None]
+        self.theta = np.random.rand(self.n_docs, n_topics)
+        self.theta = self.theta/self.theta.sum(axis=1)[:, None]
 
         self.topic_frequencies = np.zeros((self.n_docs, self.n_topics))
         self.word_topic_frequencies = np.zeros((self.n_vocab, self.n_topics))
@@ -118,6 +118,6 @@ class ReviewModel:
         
         for i in xrange(len(p)):
             r = r - p[i]
-            if r<=0:
+            if r <= 0:
                 return i
         raise Exception("Uh Oh... selectWithDistribution with r value %f" %r)