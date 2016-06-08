24be47a8f3c9dab7f10f9f1343889bd63f2b6d47
Aziz Enam <azizenam@gmail.com>
2016-06-08 15:23:33 -0700

    blah

--- lda/ReviewModel.py
+++ lda/ReviewModel.py
@@ -62,12 +62,14 @@ class ReviewModel:
         self.z = list()
         self.reviews = list()
         self.backgroundwords = np.zeros(self.n_vocab)
+        self.item_words = np.zeros(self.n_docs)
 
         for doc_ix in xrange(self.n_docs):
             data_review = data[doc_ix, :].toarray()[0]
             n_words = int(np.sum(data_review))
             self.z.append(np.zeros(n_words, dtype=int))
             self.reviews.append(flatten_bow(data_review))
+            self.item_words[doc_ix] = len(self.reviews[-1])
             for entry in self.reviews[-1]:
                 self.backgroundwords[entry] += 1.0
 
